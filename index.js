357ebbbc0aafa479410a64951186cde627b151de
MohamadHajjRabee <a39630595@gmail.com>
2024-09-08 09:46:51 +0300

    Removed /postAyahTwitter route and merged it with /updateActiveAyah.

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const {updateActiveAyah} = require('./updateActiveAyah.js')
 const { Pool } = require('pg');
 const cors = require('cors')
 const { TwitterApi } = require('twitter-api-v2');
-const {resolve} = require("path");
 
 const CRON_SECRET = process.env.CRON_SECRET
 const PORT = process.env.PORT || 3000;
@@ -27,23 +26,13 @@ const pool = new Pool({
 
 const app = express();
 app.use(cors())
-const DATASET_PATH = resolve(__dirname, 'dataset/quran.json')
 
-app.get('/updateActiveAyah', (req, res) => {
-    if(req.headers['authorization'] !== `Bearer ${CRON_SECRET}`){
-        res.status(401).json({message: 'Unauthorized'})
-    }else{
-        updateActiveAyah()
-            .then(r => res.status(200).send(r))
-            .catch(e => res.status(e.status).send("Error: " + e.message));
-    }
-});
-
-app.get('/postAyahTwitter', async (req, res) => {
+app.get('/updateActiveAyah', async (req, res) => {
     if(req.headers['authorization'] !== `Bearer ${CRON_SECRET}`){
         res.status(401).json({message: 'Unauthorized'})
     }else{
         try{
+            const updateRes = await updateActiveAyah()
             const {rows : dataRows} = await pool.query('SELECT * FROM data')
             if(dataRows[0]){
                 const {id} = dataRows[0]
@@ -52,14 +41,38 @@ app.get('/postAyahTwitter', async (req, res) => {
                 if(ayah){
                     const {ayah_ar} = ayah
                     await twitterClient.v2.tweet(ayah_ar);
-                    res.status(201).send('Ayah posted!');
+                    res.status(201).send('Ayah updated and posted on twitter!');
                 }
             }
+
         }catch (e) {
-            res.status(e.status).send("Error: " + e.message)
+            res.status(e.status).send(+ e.message)
+
         }
     }
 });
+
+// app.get('/postAyahTwitter', async (req, res) => {
+//     if(req.headers['authorization'] !== `Bearer ${CRON_SECRET}`){
+//         res.status(401).json({message: 'Unauthorized'})
+//     }else{
+//         try{
+//             const {rows : dataRows} = await pool.query('SELECT * FROM data')
+//             if(dataRows[0]){
+//                 const {id} = dataRows[0]
+//                 const {rows : quranRows} = await pool.query('SELECT * FROM quran WHERE id = $1', [id])
+//                 const ayah = quranRows[0]
+//                 if(ayah){
+//                     const {ayah_ar} = ayah
+//                     await twitterClient.v2.tweet(ayah_ar);
+//                     res.status(201).send('Ayah posted!');
+//                 }
+//             }
+//         }catch (e) {
+//             res.status(e.status).send("Error: " + e.message)
+//         }
+//     }
+// });
 app.get('/ayah', async (req, res) => {
     try {
         const {rows : dataRows} = await pool.query('SELECT * FROM data')

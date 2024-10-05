15bbf866d1c2172ba82b1185f3b26e4e8854986b
MohamadHajjRabee <a39630595@gmail.com>
2024-10-06 00:06:49 +0300

    Add the ability to post ayah on twitter with a background image.

---
+++ generateImage.js
@@ -0,0 +1,89 @@
+const {loadImage, createCanvas, registerFont} = require("canvas");
+const {join} = require("path");
+
+function wrapText(ctx, text, maxWidth) {
+    const words = text.split(' ');
+    let lines = [];
+    let line = '';
+
+    for (let n = 0; n < words.length; n++) {
+        const testLine = line + words[n] + ' ';
+        const metrics = ctx.measureText(testLine);
+        const testWidth = metrics.width;
+
+        if (testWidth > maxWidth && n > 0) {
+            lines.push(line);
+            line = words[n] + ' ';
+        } else {
+            line = testLine;
+        }
+    }
+    lines.push(line);
+    return lines;
+}
+
+function drawRoundedRect(ctx, x, y, width, height, radius) {
+    ctx.beginPath();
+    ctx.moveTo(x + radius, y);
+    ctx.lineTo(x + width - radius, y);
+    ctx.quadraticCurveTo(x + width, y, x + width, y + radius);
+    ctx.lineTo(x + width, y + height - radius);
+    ctx.quadraticCurveTo(x + width, y + height, x + width - radius, y + height);
+    ctx.lineTo(x + radius, y + height);
+    ctx.quadraticCurveTo(x, y + height, x, y + height - radius);
+    ctx.lineTo(x, y + radius);
+    ctx.quadraticCurveTo(x, y, x + radius, y);
+    ctx.closePath();
+    ctx.fill();
+}
+
+const generateImage = async (image, ayah) => {
+
+    try {
+        const background = await loadImage(image);
+        const canvas = createCanvas(background.width, background.height);
+        const ctx = canvas.getContext('2d');
+        ctx.drawImage(background, 0, 0, background.width, background.height);
+
+        const fontSize = Math.floor(background.width * 0.05)
+        const text = ayah
+        const maxWidth = background.width - 100;
+        const lineHeight = fontSize * 2.2;
+        const padding = 20;
+        const radius = 25;
+        registerFont(join(__dirname, 'ScheherazadeNew-Medium.ttf'), { family: 'ScheherazadeNew-Medium' })
+        ctx.font = `${fontSize}px "ScheherazadeNew-Medium"`;
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+
+        const lines = wrapText(ctx, text, maxWidth);
+        const backgroundWidth = ctx.measureText(lines[0]).width;
+
+        const textBlockHeight = lines.length * lineHeight + padding;
+        const textBlockWidth = backgroundWidth + padding;
+
+        const textX = canvas.width / 2;
+        const textY = canvas.height / 2 - (textBlockHeight / 2) + (lineHeight / 2);
+
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+        drawRoundedRect(
+            ctx,
+            textX - textBlockWidth / 2,
+            textY - lineHeight / 2 - padding / 2,
+            textBlockWidth,
+            textBlockHeight,
+            radius
+        );
+
+        lines.forEach((line, index) => {
+            const yPos = textY + (index * lineHeight);
+            ctx.fillStyle = '#ffffff';
+            ctx.fillText(line, textX, yPos);
+        });
+        return canvas.toBuffer('image/jpeg');
+    } catch (error) {
+        console.error('Error creating image: ', error);
+    }
+}
+
+module.exports = {generateImage}
--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const {updateActiveAyah} = require('./updateActiveAyah.js')
 const { Pool } = require('pg');
 const cors = require('cors')
 const { TwitterApi } = require('twitter-api-v2');
-
+const {generateImage} = require('./generateImage.js')
+const cloudinary = require ('cloudinary').v2;
+dotenv.config();
 const CRON_SECRET = process.env.CRON_SECRET
 const PORT = process.env.PORT || 3000;
 
 
-dotenv.config();
+
 const userClient = new TwitterApi({
     appKey:  process.env.API_KEY,
     appSecret:  process.env.API_SECRET,
@@ -20,6 +22,14 @@ const userClient = new TwitterApi({
 const bearer = new TwitterApi(process.env.BEARER_TOKEN);
 const twitterClient = userClient.readWrite;
 const twitterBearer = bearer.readOnly;
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URL,
 });
@@ -27,36 +37,12 @@ const pool = new Pool({
 const app = express();
 app.use(cors())
 
-app.get('/updateActiveAyah', async (req, res) => {
-    if(req.headers['authorization'] !== `Bearer ${CRON_SECRET}`){
-        res.status(401).json({message: 'Unauthorized'})
-    }else{
-        try{
-            const updateRes = await updateActiveAyah()
-            const {rows : dataRows} = await pool.query('SELECT * FROM data')
-            if(dataRows[0]){
-                const {id} = dataRows[0]
-                const {rows : quranRows} = await pool.query('SELECT * FROM quran WHERE id = $1', [id])
-                const ayah = quranRows[0]
-                if(ayah){
-                    const {ayah_ar} = ayah
-                    await twitterClient.v2.tweet(ayah_ar);
-                    res.status(201).send('Ayah updated and posted on twitter!');
-                }
-            }
-
-        }catch (e) {
-            res.status(e.status).send(+ e.message)
-
-        }
-    }
-});
-
-// app.get('/postAyahTwitter', async (req, res) => {
+// app.get('/updateActiveAyah', async (req, res) => {
 //     if(req.headers['authorization'] !== `Bearer ${CRON_SECRET}`){
 //         res.status(401).json({message: 'Unauthorized'})
 //     }else{
 //         try{
+//             const updateRes = await updateActiveAyah()
 //             const {rows : dataRows} = await pool.query('SELECT * FROM data')
 //             if(dataRows[0]){
 //                 const {id} = dataRows[0]
@@ -65,14 +51,57 @@ app.get('/updateActiveAyah', async (req, res) => {
 //                 if(ayah){
 //                     const {ayah_ar} = ayah
 //                     await twitterClient.v2.tweet(ayah_ar);
-//                     res.status(201).send('Ayah posted!');
+//                     res.status(201).send('Ayah updated and posted on twitter!');
 //                 }
 //             }
+//
 //         }catch (e) {
-//             res.status(e.status).send("Error: " + e.message)
+//             res.status(e.status).send({message: 'Error: ' + e.message})
+//
 //         }
 //     }
 // });
+
+app.get('/updateActiveAyah', async (req, res) => {
+    if(req.headers['authorization'] !== `Bearer ${CRON_SECRET}`){
+        res.status(401).json({message: 'Unauthorized'})
+    }else {
+        try {
+            const updateRes = await updateActiveAyah()
+            const {rows: dataRows} = await pool.query('SELECT * FROM data')
+            let ayah;
+            if (dataRows[0]) {
+                const {id} = dataRows[0]
+                const {rows: quranRows} = await pool.query('SELECT * FROM quran WHERE id = $1', [id])
+                ayah = quranRows[0]
+            }
+
+            const result = await cloudinary.search
+                .expression(`folder:"Ayah Every Day"`)
+                .execute();
+            const imageCount = result.total_count;
+            if (imageCount > 0 && ayah) {
+                const randomIndex = Math.floor(Math.random() * imageCount);
+                const randomImage = result.resources[randomIndex];
+                const imageBuffer = await generateImage(randomImage.secure_url, ayah.ayah_ar)
+                const base64Image = imageBuffer.toString('base64')
+                const mediaUploadResponse = await twitterClient.post('media/upload', { media_data: base64Image });
+                const mediaIdStr = mediaUploadResponse.data.media_id_string;
+                const tweet = {
+                    media_ids: [mediaIdStr],
+                };
+                const tweetResponse = await twitterClient.post('statuses/update', tweet);
+                res.send({message: 'image uploaded successfully'})
+
+            }
+
+        } catch (e) {
+            res.status(e.status).send({message: 'Error: ' + e.message})
+
+        }
+    }
+});
+
 app.get('/ayah', async (req, res) => {
     try {
         const {rows : dataRows} = await pool.query('SELECT * FROM data')
@@ -87,6 +116,7 @@ app.get('/ayah', async (req, res) => {
         res.status(e.status).json({message: 'Error: ' + e.message})
     }
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);

41146e03fefd5580ded3124b292aee40ab2a4d5f
MohamadHajjRabee <a39630595@gmail.com>
2024-10-06 23:06:47 +0300

    changed twitter post method

--- index.js
+++ index.js
@@ -79,14 +79,13 @@ app.get('/updateActiveAyah', async (req, res) => {
                 const randomIndex = Math.floor(Math.random() * result.total_count);
                 const randomImage = result.resources[randomIndex] || 'https://res.cloudinary.com/djrnhlouu/image/upload/v1728135066/Ayah%20Every%20Day/ofjnxsblalm70wag6voa.jpg';
                 const imageBuffer = await generateImage(randomImage.secure_url, ayah.ayah_ar)
-                const base64Image = imageBuffer.toString('base64')
-                console.log(base64Image[0])
-                const mediaUploadResponse = await twitterClient.post('media/upload', { media_data: base64Image });
-                const mediaIdStr = mediaUploadResponse.data.media_id_string;
+                const mediaUploadResponse = await twitterClient.v1.uploadMedia(imageBuffer);
                 const tweet = {
-                    media_ids: [mediaIdStr],
+                    media: {
+                        media_ids: [mediaUploadResponse],
+                    }
                 };
-                const tweetResponse = await twitterClient.post('statuses/update', tweet);
+                const tweetResponse = await twitterClient.v2.tweet(tweet);
                 res.send({message: 'image uploaded successfully'})
 
             }

13a846d29aa3b75d9961915c1936b262af706b0b
MohamadHajjRabee <a39630595@gmail.com>
2024-11-23 20:15:20 +0300

    Added logtail for updateActiveAyah to store cron job errors

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const {updateActiveAyah} = require('./updateActiveAyah.js')
 const { Pool } = require('pg');
 const cors = require('cors')
 const { TwitterApi, EUploadMimeType} = require('twitter-api-v2');
+
 const {generateImage} = require('./generateImage.js')
 const cloudinary = require ('cloudinary').v2;
 const CRON_SECRET = process.env.CRON_SECRET
@@ -12,6 +13,9 @@ const {submitInstagramImage} = require("./submitInstagramImage");
 const {generateStoryImage} = require("./generateStoryImage");
 const {uploadImageToImgur} = require("./uploadImageToImgur");
 const {deleteImgurImage} = require("./deleteImgurImage");
+const { Logtail } = require("@logtail/node");
+const logtail = new Logtail(process.env.LOGTAIL_SOURCE_TOKEN);
+
 const pool = new Pool({
     connectionString: process.env.POSTGRES_URL,
 });
@@ -39,51 +43,52 @@ app.use(cors())
 
 
 app.get('/updateActiveAyah', async (req, res) => {
-    if(req.headers['authorization'] !== `Bearer ${CRON_SECRET}`){
-        res.status(401).json({message: 'Unauthorized'})
-    }else {
-        try {
-            const updateRes = await updateActiveAyah()
-            const {rows: dataRows} = await pool.query('SELECT * FROM data')
-            let ayah;
-            if (dataRows[0]) {
-                const {id} = dataRows[0]
-                const {rows: quranRows} = await pool.query('SELECT * FROM quran WHERE id = $1', [id])
-                ayah = quranRows[0]
-            }
-            const result = await cloudinary.api.resources({
-                type: 'upload',
-                prefix: 'Ayah Every Day',
-                max_results: 100,
-            })
-            if (result && result.resources.length > 0 && ayah) {
-                const randomIndex = Math.floor(Math.random() * result.resources.length);
-                const randomImage = result.resources[randomIndex].secure_url || 'https://res.cloudinary.com/djrnhlouu/image/upload/v1728135066/Ayah%20Every%20Day/ofjnxsblalm70wag6voa.jpg';
-                const imageBuffer = await generateImage(randomImage, ayah.ayah_ar)
+    if (req.headers['authorization'] !== `Bearer ${CRON_SECRET}`) {
+        res.status(401).json({ message: 'Unauthorized' });
+        await logtail.warn('Unauthorized access attempt to /updateActiveAyah');
+        return;
+    }
+    try {
+        const updateRes = await updateActiveAyah();
+        const { rows: dataRows } = await pool.query('SELECT * FROM data');
+        let ayah;
 
-                const mediaUploadResponse = await twitterClient.v1.uploadMedia(imageBuffer, {mimeType: EUploadMimeType.Jpeg});
-                const tweet = {
-                    media: {
-                        media_ids: [mediaUploadResponse],
-                    }
-                };
-                const tweetResponse = await twitterClient.v2.tweet(tweet);
-
-                const {deleteHash, link} = await uploadImageToImgur(imageBuffer);
-                const instagramPostResponse = await submitInstagramImage(link, ayah, false)
-                await deleteImgurImage(deleteHash)
-
-                const storyImageBuffer = await generateStoryImage(imageBuffer)
-                const {deleteHash: storyDeleteHash, link: storyImageURL} = await uploadImageToImgur(storyImageBuffer);
-                const instagramStoryResponse = await submitInstagramImage(storyImageURL, ayah, true)
-                await deleteImgurImage(storyDeleteHash)
-
-
-                res.send({message: 'image uploaded successfully'})
-            }
-        } catch (e) {
-            res.status(e.status || 500).send({message: 'Error: ' + e.message})
+        if (dataRows[0]) {
+            const { id } = dataRows[0];
+            const { rows: quranRows } = await pool.query('SELECT * FROM quran WHERE id = $1', [id]);
+            ayah = quranRows[0];
         }
+
+        const result = await cloudinary.api.resources({
+            type: 'upload',
+            prefix: 'Ayah Every Day',
+            max_results: 100,
+        });
+
+        if (result && result.resources.length > 0 && ayah) {
+            const randomIndex = Math.floor(Math.random() * result.resources.length);
+            const randomImage = result.resources[randomIndex].secure_url ||
+                'https://res.cloudinary.com/djrnhlouu/image/upload/v1728135066/Ayah%20Every%20Day/ofjnxsblalm70wag6voa.jpg';
+            const imageBuffer = await generateImage(randomImage, ayah.ayah_ar);
+
+            const mediaUploadResponse = await twitterClient.v1.uploadMedia(imageBuffer, { mimeType: EUploadMimeType.Jpeg });
+            await twitterClient.v2.tweet({ media: { media_ids: [mediaUploadResponse] } });
+
+            const { deleteHash, link } = await uploadImageToImgur(imageBuffer);
+            const instagramPostResponse = await submitInstagramImage(link, ayah, false);
+            await deleteImgurImage(deleteHash);
+
+            const storyImageBuffer = await generateStoryImage(imageBuffer);
+            const { deleteHash: storyDeleteHash, link: storyImageURL } = await uploadImageToImgur(storyImageBuffer);
+            const instagramStoryResponse = await submitInstagramImage(storyImageURL, ayah, true);
+            await deleteImgurImage(storyDeleteHash);
+
+            res.send({ message: 'Image uploaded successfully' });
+            await logtail.info('Cron job completed successfully', { ayah });
+        }
+    } catch (error) {
+        await logtail.error('Error during cron job', { error: error.message });
+        res.status(error.status || 500).send({ message: 'Error: ' + error.message });
     }
 });
 
